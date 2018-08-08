9042191a9dc1fd39d43a8ba95563933322153fcb
jesusdasilva <jesusdasilva@gmail.com>
2018-08-07 22:03:26 -0400

    Traducción de la barra de navegación

--- src/Controller/InicioController.php
+++ src/Controller/InicioController.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/")
+ * @Route("/{_locale}/inicio")
  */
 class InicioController extends Controller
 {
